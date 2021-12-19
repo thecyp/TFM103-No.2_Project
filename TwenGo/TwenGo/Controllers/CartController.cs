f605698c139f867d59287adfbb8b2088a6ddbea6
thecyp <a105030102@gmail.com>
2021-12-19 17:44:21 +0800

    初始專案(已套版)

---
+++ TwenGo/TwenGo/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 套版測試2.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OrderDetail()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ TwenGo/TwenGo/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 套版測試2.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
