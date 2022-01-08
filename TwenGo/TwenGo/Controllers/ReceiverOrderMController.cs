6a1d89eb8ab013ee094f24b748fe363e221b3389
LAPTOP-6IR4E4AF\Tibame_T14 <chunyao0218@gmail.com>
2022-01-08 16:46:48 +0800

    0108

---
+++ TwenGo/TwenGo/Controllers/ReceiverOrderMController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TwenGo.Models;
+using TwenGo.Models.Repository;
+
+namespace TwenGo.Controllers
+{
+    public class ReceiverOrderMController : Controller
+    {
+        private readonly TwenGoContext _context;
+
+        public ReceiverOrderMController(TwenGoContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Order> orders = _context.Orders.ToList();
+            return View(orders);
+        }
+    }
+}
