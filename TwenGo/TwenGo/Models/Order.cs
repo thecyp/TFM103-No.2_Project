00744714741abf84490f8ead4c87f598772b13a3
LAPTOP-6IR4E4AF\Tibame_T14 <chunyao0218@gmail.com>
2022-01-10 20:42:55 +0800

    0110

--- TwenGo/TwenGo/Models/Order.cs
+++ TwenGo/TwenGo/Models/Order.cs
@@ -11,18 +11,29 @@ namespace TwenGo.Models
         [DisplayFormat(DataFormatString = "{0:000000}")]
         public int Id { get; set; }
 
+        [Display(Name = "訂單日期")]
         public DateTime OrderDate { get; set; }
+
+        [Display(Name = "使用者ID")]
         public string UserId { get; set; }
+        [Display(Name = "使用者名稱")]
         public string UserName { get; set; }
+        [Display(Name = "總價")]
         public int Total { get; set; }
 
         [Required]
+        [Display(Name = "接案者名稱")]
         public string ReceiverName { get; set; }
+
         [Required]
+        [Display(Name = "接案者地址")]
         public string ReceiverAdress { get; set; }
+
         [Required]
+        [Display(Name = "接案者電話")]
         public string ReceiverPhone { get; set; }
 
+        [Display(Name = "付款狀態")]
         public bool isPaid { get; set; }
 
         public string Status { get; set; }
