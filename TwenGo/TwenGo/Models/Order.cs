68b999da7f2b677e7ee28c17dd759ed198d41f18
thecyp <a105030102@gmail.com>
2022-01-12 19:02:24 +0800

    缺total

--- TwenGo/TwenGo/Models/Order.cs
+++ TwenGo/TwenGo/Models/Order.cs
@@ -8,6 +8,7 @@ namespace TwenGo.Models
 {
     public class Order
     {
+        [DisplayFormat(DataFormatString = "{0:000000}")]
         public int Id { get; set; }
 
         public DateTime OrderDate { get; set; }
@@ -23,6 +24,7 @@ namespace TwenGo.Models
         public string ReceiverAddress { get; set; }
         [Required]
         public string ReceiverPhone { get; set; }
+        public string Memo { get; set; }
 
 
         public bool isPaid { get; set; }
