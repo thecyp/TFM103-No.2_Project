d186cf2ea712566c42b1b5c7890921e15fe4b4d6
thecyp <a105030102@gmail.com>
2022-01-18 19:15:27 +0800

    add

--- TwenGo/TwenGo/Controllers/HomeController.cs
+++ TwenGo/TwenGo/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SpGatewayHelper;
+using SpGatewayHelper.Helper;
+using SpGatewayHelper.Models;
+using System;
 using System.Diagnostics;
 using TwenGo.Models.ViewModels;
 
@@ -8,14 +12,55 @@ namespace TwenGo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _key;
+        private readonly string _Iv;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _key = "poClKoJFuSuCgQfDK4ILyB4EDzpkMBqW";
+            _Iv = "CZuzunOnS2uFGmnP";
         }
 
-        public IActionResult Index()
+        public ActionResult Index()
         {
+            var tradeInfo = new TradeInfo()
+            {
+                MerchantID = "MS130230614",
+                RespondType = "JSON",
+                TimeStamp = DateTime.Now.ToUnixTimeStamp(),
+                Version = "1.5",
+                Amt = 100,
+                ItemDesc = "TTEESSTT",
+                //InstFlag="3,6",
+                //CreditRed = 0,
+                Email = "a105030102@gmail.com",
+                EmailModify = 0,
+                LoginType = 0,
+                MerchantOrderNo = DateTime.Now.Ticks.ToString(),
+                TradeLimit = 180,
+                //CVS=1,
+                //ExpireDate=DateTime.Now.ToString("yyyyMMdd"),
+                ReturnURL = "https://localhost:5001/Home/index",
+            };
+            var postData = tradeInfo.ToDictionary();
+            var cryptoHelper = new CryptoHelper(_key, _Iv);
+            var aesString = cryptoHelper.GetAesString(postData);
+            ViewData["TradeInfo"] = aesString;
+            ViewData["TradeSha"] = cryptoHelper.GetSha256String(aesString);
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Index(SpGatewayResponse response)
+        {
+            response.Key = _key;
+            response.Vi = _Iv;
+            var success = response.Validate("MS12345678");
+            if (success)
+            {
+                var tradInfoModel = response.GetResponseModel<TradeInfoModel>();
+            }
             return View();
         }
 
